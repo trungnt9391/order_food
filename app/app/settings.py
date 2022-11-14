92678c8f19c094b18de156060d37c51c7d94c2e8
trungnguyen <nguyen.thanh.trung.go@gmail.com>
2022-11-14 14:40:32 +0700

    checkin code

--- app/app/settings.py
+++ app/app/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'HOST': os.environ.get('DB_HOST'),
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
-        'PASSWORD':os.environ.get('DB_PASS'),
+        'PASSWORD': os.environ.get('DB_PASS'),
         
     }
 }
@@ -128,3 +128,4 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'core.User'
--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,33 @@
+from email.policy import default
+from enum import unique
+from statistics import mode
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+
+
+"""
+Database models
+"""
+
+from django.db import models
+from django.contrib.auth.models import(
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+class  UserManager(BaseUserManager):
+    """Manage for users."""
+    def create_user(self, email, password= None, **extra_field):
+        user = self.model(email = email, **extra_fields)
+        user.set_password(password)
+        user.save(using = self._db)
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length= 255,unique = True)
+    name = models.CharField(max_length =255)
+    is_active = models.BooleanField(default = True)
+    is_staff = models.BooleanField(default = False)
+
