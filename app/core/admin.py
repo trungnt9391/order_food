02b675e14bf2a17b3803aefe7a77237389a21d04
trungnguyen <nguyen.thanh.trung.go@gmail.com>
2022-11-14 14:47:49 +0700

    Test

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,28 @@
+
+"""
+Django admin customization.
+"""
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+from core import models
 
 # Register your models here.
+class UserAdmin(BaseUserAdmin):
+    """Defin the admin pages for users."""
+    order = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        )
+    )
+    admin.site.register(models.User, UserAdmin)
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -19,4 +19,28 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(user.email, email)
-        self.assertEqual(user.check_password(password))
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_nomalized(self):
+        """Test email is nomailized for new users."""
+
+        sample_emails = [
+            ['test1@EXAMPLE.com','test1@example.com'],
+            ['Test2@EXAMPLE.com','Test2@example.com']
+        ]
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email,'sample123')
+            self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        """"Test that creating a user without an email raises a ValueError."""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'test123')
+
+    def test_create_superuser(self):
+        """Test creating a super user."""
+        user = get_user_model().objects.create_superuser(
+            'TestUser@Example.com',
+            'password'
+            
+        )
